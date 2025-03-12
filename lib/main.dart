b2c2301fa82ad9b2a32d72e12473f14758046526
A.sherif <abdelrahman.sherif@4insite.com>
2025-03-12 02:12:18 +0200

    Lock app rotaion

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:tracker_app/widgets/expenses.dart';
 
 var kColorScheme = ColorScheme.fromSeed(
@@ -11,50 +12,54 @@ var kDarkColorScheme = ColorScheme.fromSeed(
 );
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme:ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        cardTheme: CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-            foregroundColor: kDarkColorScheme.onPrimaryContainer
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]).then((
+    fn,
+  ) {
+    runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          cardTheme: CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
+              foregroundColor: kDarkColorScheme.onPrimaryContainer,
+            ),
+          ),
+          appBarTheme: AppBarTheme(
+            backgroundColor: kDarkColorScheme.onPrimaryContainer,
+            foregroundColor: kDarkColorScheme.primaryContainer,
           ),
         ),
-         appBarTheme: AppBarTheme(
-          backgroundColor: kDarkColorScheme.onPrimaryContainer,
-          foregroundColor: kDarkColorScheme.primaryContainer,
-        )
-      ) ,
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.primaryContainer,
-        ),
-        cardTheme: CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          ),
+          cardTheme: CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+            titleLarge: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: kColorScheme.onSecondaryContainer,
+              fontSize: 16,
+            ),
           ),
         ),
-        textTheme: ThemeData().textTheme.copyWith(
-          titleLarge: TextStyle(
-            fontWeight: FontWeight.bold,
-            color: kColorScheme.onSecondaryContainer,
-            fontSize: 16,
-          ),
-        ),
+        themeMode: ThemeMode.system,
+        home: Expenses(),
       ),
-      themeMode: ThemeMode.system,
-      home: Expenses(),
-    ),
-  );
+    );
+  });
 }
--- lib/widgets/expenses_list/expenses_list.dart
+++ lib/widgets/expenses_list/expenses_list.dart
@@ -13,18 +13,37 @@ class ExpensesList extends StatelessWidget {
 
   final void Function(Expense expense) onRemoveExpense;
 
-  @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: list.length,
       itemBuilder:
           (ctx, index) => Dismissible(
+            key: ValueKey(list[index]),
+            direction:
+                DismissDirection
+                 .endToStart, // Ensures swipe only from right to left
             background: Container(
               color: Theme.of(context).colorScheme.error.withOpacity(0.75),
-              margin:  EdgeInsets.symmetric(
-                horizontal: Theme.of(context).cardTheme.margin!.horizontal ),
+              margin: EdgeInsets.symmetric(
+                horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+              ),
+              alignment: Alignment.centerRight,
+              padding: const EdgeInsets.symmetric(horizontal: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  const Icon(Icons.delete, color: Colors.white),
+                  const SizedBox(width: 8),
+                  const Text(
+                    'Delete',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ],
+              ),
             ),
-            key: ValueKey(list[index]),
             onDismissed: (direction) => onRemoveExpense(list[index]),
             child: ExpensesItem(expenseItem: list[index]),
           ),
